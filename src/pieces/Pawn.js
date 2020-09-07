83a063e370adef9ba4f1df65e818586c5f0fb7c5
cooljasonmelton <cooljasonmelton>
2020-09-07 10:32:59 -0500

    created chess board

---
+++ src/pieces/Pawn.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// styling
+import './Pawn.css';
+
+const Pawn = () => {
+  return (
+    <div className="Pawn">
+        <div className="pawn-head"></div>
+        <div className="pawn-middle"></div>
+        <div className="pawn-base"></div>
+    </div>
+  );
+}
+
+export default Pawn;
